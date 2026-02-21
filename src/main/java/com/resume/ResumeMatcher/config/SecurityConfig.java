336c6ee6c37b2b1722c3b0b4d225e81f2e8097db
Purbasa Maharana <purbasamaharana@gmail.com>
2026-02-21 21:40:58 +0530

    Update SecurityConfig.java

--- src/main/java/com/resume/ResumeMatcher/config/SecurityConfig.java
+++ src/main/java/com/resume/ResumeMatcher/config/SecurityConfig.java
@@ -1,36 +1,47 @@
 package com.resume.ResumeMatcher.config;
 
+import com.resume.ResumeMatcher.security.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http){
 
-                http
-                   .csrf(csrf -> csrf.disable())
-                   .authorizeHttpRequests(auth -> auth.requestMatchers("/customers/register", "/auth/login")
-                                          .permitAll()
-                                          .anyRequest()
-                                          .authenticated()
-                                         )
-                                          .addFilterBefore(jwtAuthFilter, UsernamePasswordAutheticationFilter.class);
-                  
-        
-           return http.build();
+          http
+                  .csrf(csrf -> csrf.disable())
+                   .authorizeHttpRequests(auth ->
+                                   auth.requestMatchers("/customers/register", "/auth/login")
+                                           .permitAll()
+                                           .anyRequest()
+                                           .authenticated()
+                   )
+                                            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+          return  http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration){
-            throws Exception{
-               return configuration.getAuthenticationManager();
-            }
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception{
+        return  configuration.getAuthenticationManager();
+    }
+
+}
+
 }
