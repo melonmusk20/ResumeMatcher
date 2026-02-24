b5d8dcbe37e04fb6a883c119a0074a0c322fdbe6
Purbasa Maharana <purbasamaharana@gmail.com>
2026-02-24 18:45:36 +0530

    Update SecurityConfig.java

--- src/main/java/com/resume/ResumeMatcher/config/SecurityConfig.java
+++ src/main/java/com/resume/ResumeMatcher/config/SecurityConfig.java
@@ -21,20 +21,18 @@ public class SecurityConfig {
     private JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-          http
-                  .csrf(csrf -> csrf.disable())
-                   .authorizeHttpRequests(auth ->
-                                   auth.
-                                           .permitAll()
-                                           .anyRequest()
-                                           .authenticated()
-)
-                                            
-          return  http.build();
-    }
+    http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/**/customers/register", "/**/auth/login").permitAll()
+            .anyRequest().authenticated()
+        )
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
+    return http.build();
+}
     @Bean
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration configuration) throws Exception{
