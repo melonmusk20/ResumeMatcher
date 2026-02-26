2df166c81d1046644a034a8b17f0efd607b7946c
Purbasa Maharana <purbasamaharana@gmail.com>
2026-02-26 17:49:59 +0530

    Add additional allowed origin for CORS configuration

--- src/main/java/com/resume/ResumeMatcher/config/CorsConfig.java
+++ src/main/java/com/resume/ResumeMatcher/config/CorsConfig.java
@@ -15,7 +15,10 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+      config.setAllowedOrigins(List.of(
+                             "http://localhost:5173",
+                        "https://skillsync-app-coral.vercel.app/"
+));;
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 
